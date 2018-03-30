821464609b09fa1eb8ed7bced6c92e06d84fb506
csmoe <35686186+csmoe@users.noreply.github.com>
2018-03-30 23:11:57 +0800

    rfcs: 0063-module-file-system-hierarchy

--- src/daily_rfcs/attributes/mod.rs
+++ src/daily_rfcs/attributes/mod.rs
@@ -1,3 +1,5 @@
 //! Rust attributes
 pub mod _0003_attribute_usage;
 pub mod _0016_more_attributes;
+pub mod _0049_match_arm_attributes;
+
--- src/daily_rfcs/mod.rs
+++ src/daily_rfcs/mod.rs
@@ -1,6 +1,10 @@
 //! Read [RFCs](https://github.com/rust-lang/rfcs.git) everyday
 
+// Sorted rfcs
 pub mod attributes;
 pub mod keywords;
 pub mod visibility;
+
+// Unsorted rfcs
+pub mod _0063_module_file_system_hierarchy;
 
