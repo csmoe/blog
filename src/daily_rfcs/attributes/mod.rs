70aa2fd819839499c137eddef95b526c11aa72de
csmoe <35686186+csmoe@users.noreply.github.com>
2018-04-10 23:26:15 +0800

    fix:typo

--- src/daily_rfcs/attributes/mod.rs
+++ src/daily_rfcs/attributes/mod.rs
@@ -1,4 +1,4 @@
-//! Rust attributes
+//! RFCs about attributes
 pub mod _0003_attribute_usage;
 pub mod _0016_more_attributes;
 pub mod _0049_match_arm_attributes;
--- src/daily_rfcs/mod.rs
+++ src/daily_rfcs/mod.rs
@@ -4,7 +4,6 @@
 pub mod attributes;
 pub mod keywords;
 pub mod visibility;
-
-// Unsorted rfcs
-pub mod _0063_module_file_system_hierarchy;
+pub mod matching;
+pub mod etc;
 
