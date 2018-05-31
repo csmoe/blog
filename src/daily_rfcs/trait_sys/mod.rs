b66dad9b08dd1fa41842f795a229549ae98e77bd
csmoe <35686186+csmoe@users.noreply.github.com>
2018-05-31 20:58:16 +0800

    mini format

--- src/daily_rfcs/trait_sys/mod.rs
+++ src/daily_rfcs/trait_sys/mod.rs
@@ -1,5 +1,5 @@
 //! RFCs about Rust trait system.
 pub mod _0087_trait_bounds_with_plus;
 pub mod _0255_object_safety;
-pub mod _0546_Self_not_sized_by_default;
 pub mod _0387_higher_ranked_trait_bounds;
+pub mod _0546_Self_not_sized_by_default;
