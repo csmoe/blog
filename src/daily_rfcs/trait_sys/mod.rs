7aac6e5ecfcb90008e702ce6d10d9416daa232c5
csmoe <35686186+csmoe@users.noreply.github.com>
2018-06-11 22:45:05 +0800

    fix: update mod name

--- src/daily_rfcs/trait_sys/mod.rs
+++ src/daily_rfcs/trait_sys/mod.rs
@@ -2,4 +2,4 @@
 pub mod _0087_trait_bounds_with_plus;
 pub mod _0255_object_safety;
 pub mod _0387_higher_ranked_trait_bounds;
-pub mod _0546_Self_not_sized_by_default;
+pub mod _0546_self_not_sized_by_default;
