951ec9f64dad6013f65c932f941f6202c68702e1
csmoe <35686186+csmoe@users.noreply.github.com>
2018-08-21 16:19:07 +0800

    rfcs: 0085-pattern-macros

---
+++ src/daily_rfcs/matching/_0085_pattern_macros.rs
@@ -0,0 +1,4 @@
+//! Allow macro expansion in patterns.
+//!
+//! [RFC: 0085-pattern-macros](https://github.com/rust-lang/rfcs/blob/master/text/0085-pattern-macros.md)
+struct RFCs(i32);
--- src/daily_rfcs/matching/mod.rs
+++ src/daily_rfcs/matching/mod.rs
@@ -1,2 +1,3 @@
 //! RFCs about pattern match.
 pub mod _0160_if_let;
+pub mod _0085_pattern_macros;
