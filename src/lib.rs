eaf3ec7e91355e2a192cbfed42ac9e0b706292a5
csmoe <csmoe@msn.com>
2022-10-15 21:25:43 +0800

    add logo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-//! Learning notes about [Rust](https://rust-lang.org)
-//!
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/35686186?v=4")]
 #![allow(unused)]
 
 pub mod daily_rfcs;
